5cb799e41fb49932b4dc724ff1081cbf6243555a
Amit Kumar <amitniitmca@gmail.com>
2023-09-30 00:08:17 +0530

    Comment added to confirmation modal js

--- force-app/main/default/lwc/confirmationModal/confirmationModal.js
+++ force-app/main/default/lwc/confirmationModal/confirmationModal.js
@@ -1,3 +1,9 @@
+/**
+ * @description       : JS file of the Confirmation Lightning Modal.
+ * @author            : Amit Kumar (Proper Salesforce Tutorials)
+ * @last modified on  : 29-09-2023
+ * @last modified by  : Amit Kumar (Proper Salesforce Tutorials)
+**/
 import { api } from 'lwc';
 import LightningModal from 'lightning/modal';
 
@@ -5,10 +11,18 @@ export default class ConfirmationModal extends LightningModal {
     @api title;
     @api content;
 
+    /**
+    * @description Handler for the Yes button click event.
+    * @author Amit Kumar (Proper Salesforce Tutorials) | 29-09-2023 
+    **/
     handleYes(){
         this.close('YES');
     }
     
+    /**
+    * @description Handler for the No button click event.
+    * @author Amit Kumar (Proper Salesforce Tutorials) | 29-09-2023 
+    **/
     handleNo(){
         this.close('NO');
     }
